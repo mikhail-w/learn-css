463f930c0eee3cffd17e3aab09fdfc138695960e
Mikhail Waddell <mikhail.waddell@gmail.com>
2024-04-27 15:26:53 -0400

    css grid

--- web-dev/script.js
+++ web-dev/script.js
@@ -1,41 +1,22 @@
-const awesome = document.querySelector('[data-element="awesome"]');
+const parentBox = document.querySelector('[data-element="parent-box"]');
+const heightLabel = document.querySelector('[data-element="height-label"]');
+const widthLabel = document.querySelector('[data-element="width-label"]');
 const intrinsicSwitch = document.querySelector('#intrinsic-switch');
-const paragraph = document.querySelector('pre');
 
-const text = `
-        <pre>
-          This is a pre formatted
+// Update the size labels on resize
+const observer = new ResizeObserver(observedItems => {
+  const { borderBoxSize } = observedItems[0];
 
-          sentence and will be 
+  widthLabel.innerText = `${Math.round(borderBoxSize[0].inlineSize)}px`;
+  heightLabel.innerText = `${Math.round(borderBoxSize[0].blockSize)}px`;
+});
 
-          seen just the way it was typed
-
-          I CAN DO IT!!!!
-
-          yES i can~ ! ! - ==+
-        </pre>
-`;
+observer.observe(parentBox);
 
 // Set sizing attribute based on switch
 intrinsicSwitch.addEventListener('change', () => {
-  awesome.setAttribute(
+  parentBox.setAttribute(
     'data-sizing',
     intrinsicSwitch.checked ? 'intrinsic' : 'extrinsic'
   );
-});
-
-paragraph.addEventListener('mouseenter', e => {
-  // console.log(e.target.innerHTML);
-  let temp = e.target.innerHTML;
-  let rep = `
-  <pre><bdo dir="rtl">
-  ${temp}
-  </bdo></pre>
-  `;
-  // console.log(rep);
-  paragraph.innerHTML = rep;
-});
-
-paragraph.addEventListener('mouseleave ', e => {
-  paragraph.innerHTML = text;
 });
